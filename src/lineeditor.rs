ec1ab922c743194ed97bd5b34de10db938e1b62d
Leon Kowarschick <lkowarschick@gmail.com>
2020-04-09 14:59:50 +0200

    implement it correctly

--- src/lineeditor.rs
+++ src/lineeditor.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use unicode_segmentation::*;
+use unicode_width::*;
 #[derive(Debug, Clone)]
 pub struct EditorState {
     content: String,
@@ -25,58 +25,53 @@ impl EditorState {
     }
 
     pub fn set_content(&mut self, new_content: &str) {
-        //self.content = UnicodeSegmentation::graphemes(new_content, true)
-        //.map(|x| x.to_string())
-        //.collect::<Vec<String>>();
-        //self.cursor_col = new_content.len();
-        unimplemented!();
+        self.content = new_content.to_owned();
+        self.cursor_col = self.content.len();
     }
 
     pub fn content_str(&self) -> String { self.content.to_owned() }
+    pub fn displayed_cursor_column(&self) -> usize { UnicodeWidthStr::width(&self.content[..self.cursor_col]) }
 
-    pub fn displayed_cursor_column(&self) -> usize {
-        self.content
-            .chars()
-            .take(self.cursor_col)
-            .filter_map(unicode_width::UnicodeWidthChar::width)
-            .sum::<usize>()
+    fn next_char_index(&self) -> usize {
+        if self.cursor_col == self.content.len() {
+            return self.cursor_col;
+        }
+        let mut new_cursor = self.cursor_col + 1;
+        while let None = self.content_str().get(new_cursor..) {
+            new_cursor += 1;
+        }
+        new_cursor
+    }
+    fn prev_char_index(&self) -> usize {
+        if self.cursor_col == 0 {
+            return 0;
+        }
+        let mut new_cursor = self.cursor_col - 1;
+        while let None = self.content_str().get(new_cursor..) {
+            new_cursor -= 1;
+        }
+        new_cursor
     }
 
     pub fn apply_event(&mut self, event: EditorEvent) {
         match event {
             EditorEvent::NewCharacter(c) => {
-                let c_string = c.to_string();
-                let mut grapheme_vec: Vec<&str> = self.content.graphemes(true).collect_vec();
-                grapheme_vec.insert(self.cursor_col, &c_string);
-                self.content = grapheme_vec.join("");
-                self.cursor_col += 1;
+                self.content.insert(self.cursor_col, c);
+                self.cursor_col = self.next_char_index();
             }
             EditorEvent::Backspace if self.cursor_col > 0 => {
-                let mut grapheme_vec: Vec<&str> = self.content.graphemes(true).collect_vec();
-                let deleted_grapheme_len = grapheme_vec[self.cursor_col - 1].len();
-                grapheme_vec.remove(self.cursor_col - 1);
-                self.content = grapheme_vec.join("");
-                self.cursor_col -= deleted_grapheme_len;
+                let new_cursor = self.prev_char_index();
+                self.content.remove(new_cursor);
+                self.cursor_col = new_cursor;
             }
             EditorEvent::Delete if self.cursor_col < self.content.len() => {
-                let mut grapheme_vec: Vec<&str> = self.content.graphemes(true).collect_vec();
-                let deleted_grapheme_len = self
-                    .content
-                    .chars()
-                    .skip(self.cursor_col)
-                    .collect::<String>()
-                    .graphemes(true)
-                    .take(1)
-                    .join("")
-                    .len();
-                self.content = grapheme_vec.join("");
-                self.cursor_col -= deleted_grapheme_len;
+                self.content.remove(self.cursor_col);
             }
             EditorEvent::GoLeft if self.cursor_col > 0 => {
-                self.cursor_col -= 1;
+                self.cursor_col = self.prev_char_index();
             }
             EditorEvent::GoRight if self.cursor_col < self.content.len() => {
-                self.cursor_col += 1;
+                self.cursor_col = self.next_char_index();
             }
             EditorEvent::Home => {
                 self.cursor_col = 0;
@@ -85,16 +80,13 @@ impl EditorState {
                 self.cursor_col = self.content.len();
             }
             EditorEvent::KillWordBack if !self.content.is_empty() => {
-                //let mut i = self.content.len() - 1;
-                //while let Some(c) = self.content.clone().get(i) {
-                //self.content.remove(i);
-                //self.cursor_col -= 1;
-                //if c == " " || c == "/" || c == "\\" || c == ":" || c == "_" || c == "-" || i == 0 {
-                //break;
-                //};
-                //i -= 1;
-                //}
-                unimplemented!()
+                while let Some(c) = self.content.to_owned().get(self.prev_char_index()..self.cursor_col) {
+                    self.cursor_col = self.prev_char_index();
+                    self.content.remove(self.cursor_col);
+                    if c == " " || c == "/" || c == "\\" || c == ":" || c == "_" || c == "-" || self.cursor_col == 0 {
+                        break;
+                    }
+                }
             }
             _ => {}
         }
@@ -111,6 +103,7 @@ pub mod test {
 
         le.apply_event(EditorEvent::NewCharacter('a'));
         assert_eq!(le.content_str(), "a");
+
         le.apply_event(EditorEvent::NewCharacter('a'));
         assert_eq!(le.content_str(), "aa");
         assert_eq!(le.displayed_cursor_column(), 2);
@@ -144,15 +137,38 @@ pub mod test {
     }
 
     #[test]
+    pub fn test_advanced() {
+        let mut le = EditorState::new();
+        le.set_content("as");
+        assert_eq!(le.content_str(), "as");
+        assert_eq!(le.displayed_cursor_column(), 2 as usize);
+
+        le.apply_event(EditorEvent::KillWordBack);
+        assert_eq!(le.content_str(), "");
+        assert_eq!(le.displayed_cursor_column(), 0 as usize);
+
+        le.set_content("as as as");
+        assert_eq!(le.content_str(), "as as as");
+        assert_eq!(le.displayed_cursor_column(), 8 as usize);
+
+        le.apply_event(EditorEvent::KillWordBack);
+        assert_eq!(le.content_str(), "as as");
+        assert_eq!(le.displayed_cursor_column(), 5 as usize);
+    }
+
+    #[test]
     pub fn test_lineeditor_umlaut() {
         let mut le = EditorState::new();
         assert_eq!(le.content_str(), "");
 
         le.apply_event(EditorEvent::NewCharacter('ä'));
         assert_eq!(le.content_str(), "ä");
-        dbg!(le.content_str().graphemes(true).collect_vec());
+        assert_eq!(le.displayed_cursor_column(), 1);
         le.apply_event(EditorEvent::NewCharacter('ä'));
         assert_eq!(le.content_str(), "ää");
         assert_eq!(le.displayed_cursor_column(), 2);
+
+        le.apply_event(EditorEvent::GoLeft);
+        assert_eq!(le.displayed_cursor_column(), 1);
     }
 }
