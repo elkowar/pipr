516883007c3972a0b3e353b57dca89d42e044fcc
Leon Kowarschick <lkowarschick@gmail.com>
2020-04-13 17:44:22 +0200

    use stable in build_docker

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:nightly-2020-04-10 cargo build --release
+docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release
