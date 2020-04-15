c2606d552aa209af862f7c313730370716315a80
Leon Kowarschick <lkowarschick@gmail.com>
2020-04-15 18:34:29 +0200

    update crossterm and tui-rs and implement man-page fully

--- src/ui.rs
+++ src/ui.rs
@@ -1,15 +1,17 @@
 use super::app::*;
+use crate::snippets::Snippet;
 use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
 use std::{
+    collections::HashMap,
     io::{self, Stdout, Write},
     process::Command,
 };
 use tui::layout::{Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, List, Paragraph, SelectableList, Text, Widget};
+use tui::widgets::{Block, Borders, List, ListState, Paragraph, Text};
 use tui::{backend::Backend, backend::CrosstermBackend, Frame, Terminal};
 use Constraint::*;
 
@@ -23,14 +25,13 @@ fn make_default_block(title: &str, selected: bool) -> Block {
     Block::default().title(title).borders(Borders::ALL).title_style(title_style)
 }
 
-pub fn draw_app(mut terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut App) -> Result<(), failure::Error> {
+pub fn draw_app(terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut App) -> Result<(), failure::Error> {
     if let Some(manpage) = &app.opened_manpage {
         execute!(io::stdout(), LeaveAlternateScreen)?;
-        Command::new("man").arg(manpage).spawn().unwrap().wait().unwrap();
-        execute!(io::stdout(), EnterAlternateScreen)?;
-        std::io::Write::flush(&mut terminal.backend_mut())?;
+        Command::new("man").arg(manpage).spawn()?.wait()?;
         app.opened_manpage = None;
-        return Ok(());
+        execute!(io::stdout(), EnterAlternateScreen)?;
+        terminal.resize(terminal.size()?)?;
     }
 
     let mut input_field_rect = tui::layout::Rect::new(0, 0, 0, 0);
@@ -44,20 +45,14 @@ pub fn draw_app(mut terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut
                     .constraints([Percentage(if app.snippet_mode { 40 } else { 0 }), Percentage(100)].as_ref())
                     .split(root_rect);
 
-                let snippet_list = app
-                    .config
-                    .snippets
-                    .iter()
-                    .map(|(c, snippet)| c.to_string() + ": " + &snippet.text.trim());
-
-                List::new(snippet_list.map(Text::raw))
-                    .block(make_default_block("Snippets", false))
-                    .render(&mut f, root_chunks[0]);
-
                 let exec_chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .constraints([Length(2 + app.input_state.content_lines().len() as u16), Percentage(100)].as_ref())
                     .split(root_chunks[1]);
+
+                if app.snippet_mode {
+                    draw_snippet_list(&mut f, root_chunks[0], &app.config.snippets);
+                }
 
                 input_field_rect = exec_chunks[0];
                 draw_input_field(&mut f, input_field_rect, &app);
@@ -70,9 +65,11 @@ pub fn draw_app(mut terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut
                 );
             }
             WindowState::TextView(title, text) => {
-                Paragraph::new([Text::raw(text)].iter())
-                    .block(make_default_block(title, true))
-                    .render(&mut f, root_rect);
+                let paragraph_content = [Text::raw(text)];
+                f.render_widget(
+                    Paragraph::new(paragraph_content.iter()).block(make_default_block(title, true)),
+                    root_rect,
+                );
             }
             WindowState::BookmarkList(listview_state) => {
                 let always_show_preview = app.config.cmdlist_always_show_preview;
@@ -84,29 +81,25 @@ pub fn draw_app(mut terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut
             }
         }
 
-        Paragraph::new([Text::raw("Help: F1")].iter())
-            .render(&mut f, Rect::new(root_rect.width - 10 as u16, root_rect.height as u16, 10, 1))
+        f.render_widget(
+            Paragraph::new([Text::raw("Help: F1")].iter()),
+            Rect::new(root_rect.width - 10 as u16, root_rect.height as u16, 10, 1),
+        );
     })?;
 
     match app.window_state {
         WindowState::Main => {
-            set_crossterm_cursor_visibility(&mut terminal, true);
+            terminal.show_cursor()?;
             let cursor_x = input_field_rect.x + 1 + app.input_state.displayed_cursor_column() as u16;
             let cursor_y = input_field_rect.y + 1 + app.input_state.cursor_line as u16;
-            set_crossterm_cursor_position(&mut terminal, cursor_x, cursor_y);
+            terminal.set_cursor(cursor_x, cursor_y)?;
         }
-        _ => set_crossterm_cursor_visibility(&mut terminal, false),
+        _ => terminal.hide_cursor()?,
     }
     Ok(())
 }
 
-fn draw_command_list<B: Backend>(
-    mut f: &mut Frame<B>,
-    rect: Rect,
-    always_show_preview: bool,
-    state: &CommandListState,
-    title: &str,
-) {
+fn draw_command_list<B: Backend>(f: &mut Frame<B>, rect: Rect, always_show_preview: bool, state: &CommandListState, title: &str) {
     let show_preview = always_show_preview || state.selected_entry().map(|e| e.lines().len() > 1) == Some(true);
 
     let chunks = Layout::default()
@@ -120,24 +113,27 @@ fn draw_command_list<B: Backend>(
         .map(|entry| str::replace(&entry.as_string(), "\n", " ↵ "))
         .collect::<Vec<String>>();
 
-    SelectableList::default()
+    let mut list_state = ListState::default();
+    list_state.select(state.selected_idx);
+
+    let list_widget = List::new(items.iter().map(Text::raw))
         .block(make_default_block(title, true))
-        .items(items.as_slice())
-        .select(state.selected_idx)
         .highlight_style(Style::default().modifier(Modifier::ITALIC))
-        .highlight_symbol(">>")
-        .render(&mut f, chunks[0]);
+        .highlight_symbol(">>");
+
+    f.render_stateful_widget(list_widget, chunks[0], &mut list_state);
 
     if show_preview {
         if let Some(selected_content) = state.selected_entry() {
-            Paragraph::new([Text::raw(selected_content.as_string())].iter())
-                .block(make_default_block("Preview", false))
-                .render(&mut f, chunks[1]);
+            f.render_widget(
+                Paragraph::new([Text::raw(selected_content.as_string())].iter()).block(make_default_block("Preview", false)),
+                chunks[1],
+            );
         }
     }
 }
 
-fn draw_input_field<B: Backend>(mut f: &mut Frame<B>, rect: Rect, app: &App) {
+fn draw_input_field<B: Backend>(f: &mut Frame<B>, rect: Rect, app: &App) {
     let lines = app.input_state.content_lines().iter().map(|line| {
         let mut line = line.clone();
         if line.len() > rect.width as usize - 5 {
@@ -147,25 +143,35 @@ fn draw_input_field<B: Backend>(mut f: &mut Frame<B>, rect: Rect, app: &App) {
         line
     });
     let is_bookmarked = app.bookmarks.entries.contains(&app.input_state.content_to_commandentry());
+    let input_block_title = format!(
+        "Command{}{}{}",
+        if is_bookmarked { " [Bookmarked]" } else { "" },
+        if app.autoeval_mode { " [Autoeval]" } else { "" },
+        if app.autoeval_mode && app.paranoid_history_mode {
+            " [Paranoid]"
+        } else {
+            ""
+        }
+    );
 
-    List::new(lines.map(Text::raw))
-        .block(make_default_block(
-            &format!(
-                "Command{}{}{}",
-                if is_bookmarked { " [Bookmarked]" } else { "" },
-                if app.autoeval_mode { " [Autoeval]" } else { "" },
-                if app.autoeval_mode && app.paranoid_history_mode {
-                    " [Paranoid]"
-                } else {
-                    ""
-                },
-            ),
-            true,
-        ))
-        .render(&mut f, rect);
+    f.render_widget(
+        List::new(lines.map(Text::raw)).block(make_default_block(&input_block_title, true)),
+        rect,
+    );
 }
 
-fn draw_outputs<B: Backend>(mut f: &mut Frame<B>, rect: Rect, changed: bool, stdout: &str, stderr: &str) {
+fn draw_snippet_list<B: Backend>(f: &mut Frame<B>, rect: Rect, snippets: &HashMap<char, Snippet>) {
+    let snippet_list = snippets
+        .iter()
+        .map(|(c, snippet)| c.to_string() + ": " + &snippet.text.trim());
+
+    f.render_widget(
+        List::new(snippet_list.map(Text::raw)).block(make_default_block("Snippets", false)),
+        rect,
+    );
+}
+
+fn draw_outputs<B: Backend>(f: &mut Frame<B>, rect: Rect, changed: bool, stdout: &str, stderr: &str) {
     let output_chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints(if stderr.is_empty() {
@@ -175,33 +181,17 @@ fn draw_outputs<B: Backend>(mut f: &mut Frame<B>, rect: Rect, changed: bool, std
         })
         .split(rect);
 
-    Paragraph::new([Text::raw(stdout)].iter())
-        .block(make_default_block(
-            &format!("Output{}", if changed { "" } else { " [+]" }),
-            false,
-        ))
-        .render(&mut f, output_chunks[0]);
+    let stdout_item = [Text::raw(stdout)];
+    let stdout_title = format!("Output{}", if changed { "" } else { " [+]" });
+    f.render_widget(
+        Paragraph::new(stdout_item.iter()).block(make_default_block(&stdout_title, false)),
+        output_chunks[0],
+    );
 
     if !stderr.is_empty() {
-        Paragraph::new([Text::raw(stderr)].iter())
-            .block(make_default_block("Stderr", false))
-            .render(&mut f, output_chunks[1]);
+        f.render_widget(
+            Paragraph::new([Text::raw(stderr)].iter()).block(make_default_block("Stderr", false)),
+            output_chunks[1],
+        );
     }
-}
-
-fn set_crossterm_cursor_position(terminal: &mut Terminal<CrosstermBackend<Stdout>>, x: u16, y: u16) {
-    terminal
-        .backend_mut()
-        .write(format!("{}", crossterm::cursor::MoveTo(x, y)).as_bytes())
-        .unwrap();
-    io::stdout().flush().ok();
-}
-
-fn set_crossterm_cursor_visibility(terminal: &mut Terminal<CrosstermBackend<Stdout>>, visible: bool) {
-    let command = match visible {
-        true => format!("{}", crossterm::cursor::Show),
-        false => format!("{}", crossterm::cursor::Hide),
-    };
-    terminal.backend_mut().write(command.as_bytes()).unwrap();
-    io::stdout().flush().ok();
 }
