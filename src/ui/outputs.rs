f63a477c64db266a02de5f5c8f97a54e98395b90
elkowar <dev@elkowar.dev>
2025-05-03 19:32:32 +0200

    Make stdout take up the full screen when possible

--- src/ui/outputs.rs
+++ src/ui/outputs.rs
@@ -12,27 +12,31 @@ use crate::ui::{display_processing_state, make_default_block};
 pub fn draw_outputs(f: &mut Frame, rect: Rect, changed: bool, processing_state: Option<u8>, stdout: &str, stderr: &str) {
     let text = stdout.into_text().unwrap_or_else(|_| Text::raw(stdout));
 
-    let output_chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Percentage(if stderr.is_empty() { 100 } else { 50 }), Percentage(100)].as_ref())
-        .split(rect);
-
     let stdout_title = format!(
         "Output{}{}",
         if changed { "" } else { " [+]" },
         display_processing_state(processing_state)
     );
 
+    let [stdout_chunk, stderr_chunk] = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(if stderr.is_empty() {
+            [Percentage(100), Percentage(0)].as_ref()
+        } else {
+            [Percentage(50), Percentage(50)].as_ref()
+        })
+        .areas(rect);
+
     f.render_widget(
         Paragraph::new(text).block(make_default_block(&stdout_title, false)),
-        output_chunks[0],
+        stdout_chunk,
     );
 
     if !stderr.is_empty() {
         let stderr_text = stderr.into_text().unwrap_or_else(|_| Text::raw(stderr));
         f.render_widget(
             Paragraph::new(stderr_text).block(make_default_block("Stderr", false)),
-            output_chunks[1],
+            stderr_chunk,
         );
     }
 }
